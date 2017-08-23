16c92ad74c643b49382e81a7390380dd1bd3c548
Reginaldo Morikawa <reginaldo@wolfandpartners.com.br>
2017-08-23 08:30:49 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lista21082017ex06
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            decimal fahrenheit, celsius;
+
+            Console.WriteLine("Digite a temperatuda em Fahrenheit: ");
+            fahrenheit = Convert.ToDecimal(Console.ReadLine());
+            celsius = ((fahrenheit - 32) * 5) / 9;
+            Console.WriteLine("A temperatura em celsius é {0:###,##0.00}", celsius);
+        }
+    }
+}
